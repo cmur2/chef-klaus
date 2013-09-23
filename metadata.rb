a1859133705156c3c3ea9d4723061cdcac5b9058
cn <cn@mycrobase.de>
2013-09-23 20:40:02 +0200

    Update

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             "klaus"
 maintainer       "Christian Nicolai"
 maintainer_email "cn@mycrobase.de"
 license          "Apache 2.0"
-description      ""
+description      "Installs klaus (the git web viewer) via pip and configures it"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
